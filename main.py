8bb5e6f485b048bd03449f818048a692405219d2
SuzukiTomoyuki <dxfsrs1004@gmail.com>
2015-10-06 13:02:19 +0900

    鼻眼鏡君完成

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 
 import cv2
 
-cascade = cv2.CascadeClassifier('cascade_filter/haarcascade_frontalface_default.xml')
+def CamCapture(mirror = False, size = (1280, 720)):
+    cascade = cv2.CascadeClassifier('cascade_filter/haarcascade_frontalface_default.xml')
+  
+    dst = cv2.imread('image/megane.png', -1)
 
-def CamCapture(mirror = False, size = (1200, 900)):
     cap = cv2.VideoCapture(0)
 
     while(1):
@@ -14,14 +16,25 @@ def CamCapture(mirror = False, size = (1200, 900)):
         face = cascade.detectMultiScale(gray, 1.1, 3)
 
         for (x, y, w, h) in face:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 50, 255), 3)
+            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 50, 255), 3)
 
-
+            dst2 = cv2.resize(dst, (w,h))
+            d_w, d_h = dst2.shape[:2]
+            mask = dst2[:,:,3]
+            mask = cv2.cvtColor(mask, cv2.cv.CV_GRAY2BGR)
+            mask = mask / 255.0
+            dst2 = dst2[:, :, :3]
+            try:
+                frame[y:d_h + y:, x:d_w + x] *= 1 - mask
+                frame[y:d_h + y:, x:d_w + x] += dst2 * mask
+            except Exception, e:
+                pass
+        
         if size is not None and len(size) == 2:
             frame = cv2.resize(frame, size)
-            
+
         if ret == True:
-            cv2.imshow('frame', frame)
+            cv2.imshow(u'鼻眼鏡君'.encode('shift-jis'), frame)
             key = cv2.waitKey(1)
             if key == ord('q'):
                 break
